e276f71ac01891a34ff1d2bed182505c0be7c59b
Calvyn <list.black32@yahoo.com>
2018-07-24 12:17:15 +0700

    Finish example

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,37 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 //    final wordPair = WordPair.random();
     return MaterialApp(
         title: 'Welcome to Flutter',
-        home: RandomWords()
-    );
+        theme: ThemeData(primaryColor: Color(0xFF069ff9)),
+        home: RandomWords());
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   createState() => RandomWordState();
 }
 
-class RandomWordState extends State<RandomWords>{
+class RandomWordState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerfont= const TextStyle(fontSize: 18.0);
+  final _biggerfont = const TextStyle(fontSize: 18.0);
+  final _headerfont = const TextStyle(color: Colors.white);
+
   final _saved = Set<WordPair>();
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context,i){
-        if(i.isOdd)
-          return Divider();
-        final index = i ~/2;
-        if(index>= _suggestions.length){
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
@@ -39,7 +41,7 @@ class RandomWordState extends State<RandomWords>{
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
@@ -47,14 +49,14 @@ class RandomWordState extends State<RandomWords>{
         style: _biggerfont,
       ),
       trailing: Icon(
-        alreadySaved?Icons.favorite:Icons.favorite_border,
-        color: alreadySaved?Colors.red:null,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
-      onTap: (){
+      onTap: () {
         setState(() {
-          if(alreadySaved){
+          if (alreadySaved) {
             _saved.remove(pair);
-          }else{
+          } else {
             _saved.add(pair);
           }
         });
@@ -67,9 +69,38 @@ class RandomWordState extends State<RandomWords>{
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup name generator'),
+        title: Text('Startup name generator', style: _headerfont),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.list, color: Colors.white),
+              onPressed: _pushSaved)
+        ],
+        centerTitle: true,
       ),
       body: _buildSuggestions(),
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerfont,
+          ),
+        );
+      });
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions', style: _headerfont),
+          centerTitle: true,
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 }
